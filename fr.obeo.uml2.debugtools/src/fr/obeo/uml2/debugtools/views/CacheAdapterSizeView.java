db08928b7590b9e074ac9cbcea4cf9e86a809713
Mikaël Barbero <mikael.barbero@gmail.com>
2012-10-11 17:45:38 +0200

    add license & author

--- fr.obeo.uml2.debugtools/src/fr/obeo/uml2/debugtools/views/CacheAdapterSizeView.java
+++ fr.obeo.uml2.debugtools/src/fr/obeo/uml2/debugtools/views/CacheAdapterSizeView.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package fr.obeo.uml2.debugtools.views;
 
 
@@ -17,7 +27,9 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.uml2.common.util.CacheAdapter;
 
-
+/**
+ * @author <a href="mailto:mikael.barbero@obeo.fr">Mikael Barbero</a>
+ */
 public class CacheAdapterSizeView extends ViewPart {
 
 	private Label sizeDisplay;
